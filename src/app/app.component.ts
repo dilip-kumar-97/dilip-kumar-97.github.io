948839d3a815615dc1ebe2b50e4a2e0057865f78
siranjeevi <siranjeeveei1998@gmail.com>
2023-08-17 13:01:03 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dilip-kumar-97.github.io';
+}
